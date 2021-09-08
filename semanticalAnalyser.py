df91b0ad196ff4ff7123f9add5e9af64b092d350
VictorPozzan <victor_pozzan@hotmail.com>
2021-09-08 18:10:14 -0300

    feat: stmt semantical analiser

--- semanticalAnalyser.py
+++ semanticalAnalyser.py
@@ -26,6 +26,7 @@ class Semantic_Analyser:
         
         #verificar condicoes de if
         #verificar condicoes de while
+        self.verify_statements()
 
     def insert_deep_structure(self):
         list_count_id = []
@@ -41,7 +42,6 @@ class Semantic_Analyser:
             
             elif token[0] == '{}}':
                 list_count_id.pop(-1)  
-
         
         
     def construct_variable_table(self):
@@ -99,25 +99,35 @@ class Semantic_Analyser:
                     self.printErro(errormessage, token[2])
 
 
-
     def verify_type(self, token_receiver, token_attributor):
-        if token_attributor[0] == "{ID}":
-            type_var = list(filter(lambda x:token_attributor[1] in x, self.list_var))
-            type_attributor = type_var[0]
-        elif token_attributor[0] == "{NUM_INT}":
-            type_attributor = [('{INT}')]
-        elif token_attributor[0] == "{NUM_FLOAT}":
-            type_attributor = [('{FLOAT}')]
-        elif token_attributor[0] == "{FALSE}" or token_attributor[0] == "{TRUE}":
-            type_attributor = [('{BOOL}')]
-        else :
-            type_attributor = [token_attributor[0]]
+        type_attributor = self.get_type_token(token_attributor)
 
-        type_receiver = list(filter(lambda x:token_receiver[1] in x, self.list_var))
-        if type_receiver[0][0] == type_attributor[0]:
+        if token_receiver[0] == '{ID}':
+            exists = list(filter(lambda x:token_receiver[1] in x, self.list_var))
+            type_receiver = exists[0][0]
+        else:
+            receiver = self.get_type_token(token_receiver)
+            type_receiver = receiver[0]
+
+        if type_receiver == type_attributor[0]:
             return
         else:
-            print("ERRO VARIAVEIS NAO SAO DO MESMO TIPO, ERA ESPERADO", type_receiver[0][0],  "E RECEBEMOS",  type_attributor[0], "linha", token_attributor[2])
+            print("ERRO VARIAVEIS NAO SAO DO MESMO TIPO, ERA ESPERADO", type_receiver,  "E RECEBEMOS",  type_attributor[0], "linha", token_attributor[2])
+
+    def get_type_token(self, token):
+        if token[0] == "{ID}":
+            type_var = list(filter(lambda x:token[1] in x, self.list_var))
+            type_attributor = type_var[0]
+        elif token[0] == "{NUM_INT}":
+            type_attributor = [('{INT}')]
+        elif token[0] == "{NUM_FLOAT}":
+            type_attributor = [('{FLOAT}')]
+        elif token[0] == "{FALSE}" or token[0] == "{TRUE}":
+            type_attributor = [('{BOOL}')]
+        else :
+            type_attributor = [token[0]]
+        
+        return type_attributor
 
     def verify_attributor(self, token_receiver, token_attributor):
         if token_attributor[0] == "{ID}":
@@ -134,6 +144,54 @@ class Semantic_Analyser:
         else: 
             self.verify_type(token_receiver, token_attributor)
     
+
+    def verify_statements(self):
+        print()
+        print()
+        for i, token in enumerate(self.tokens_deep):
+            if token[0] == '{IF}' or token[0] ==  '{WHILE}':
+                token_one = self.tokens_deep[i + 2]
+                self.verify_scope_id(token_one)
+                if token_one[0] == '{NOT}':
+                    print("stmt do not") 
+                    token_two = self.tokens_deep[i + 3]
+                    if token_two[0] == '{ID}':
+                        self.verify_scope_id(token_two)
+                        type_token = self.get_type_token(token_two)
+                        if type_token[0] != '{BOOL}':
+                            print("ERRRRRRRRRROOOOO  era esperado um ID do tipo boolean    linha", token_two[2] )
+                    else:
+                        print("ERRRRRRRRRROOOOO  era esperado um ID do tipo boolean    linha", token_two[2] )
+                else:
+                    #se é um ID ou um NUM_INT, NUM_FLOAT, FALSE, ou TRUE
+                    token_two = self.tokens_deep[i + 4]
+                    self.verify_scope_id(token_two)
+                    print("é um ",token[0], "os tokens são:", token_one, token_two)
+                    self.verify_attributor(token_one, token_two)
+                    #verificar
+                    token_exists_op_logico = self.tokens_deep[i + 5]
+                    if token_exists_op_logico[0] == '{AND}' or token_exists_op_logico[0] == '{OR}':
+                        token_four = self.tokens_deep[i + 6]
+                        token_five = self.tokens_deep[i + 8]
+                        self.verify_scope_id(token_four)
+                        self.verify_scope_id(token_five)
+                        self.verify_attributor(token_four, token_five)
+
+
+    def verify_scope_id(self, token):
+        if token[0] == '{ID}':
+            exists_id_atributor = list(filter(lambda x:token[1] in x, self.list_var))
+            if len(exists_id_atributor) == 1:
+                if token[2] >= exists_id_atributor[0][2] and exists_id_atributor[0][3] in token[4]:
+                    return True
+                else:
+                    print("ERRROOO variavel utilizada fora de contexto", exists_id_atributor[0][1])
+                    return False
+            else:
+                print("ERRROOO id não declarado", exists_id_atributor[1])
+                return False
+
+
 
     def printErro(self, error, row, color_one = '\x1b[1;37;41m', color_two = '\x1b[0m'):
         a = 0
